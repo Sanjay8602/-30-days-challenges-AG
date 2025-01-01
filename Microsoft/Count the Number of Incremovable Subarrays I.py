7b959f683d04bb976c9ca7477e6a60e310d53bda
Sanjay <sutharsh0806@gmail.com>
2025-01-01 22:09:17 +0530

    no of incremovable subarrays

---
+++ Microsoft/Count the Number of Incremovable Subarrays I.py
@@ -0,0 +1,14 @@
+class Solution:
+    def incremovableSubarrayCount(self, nums: List[int]) -> int:
+        def is_strictly_increasing(arr):
+            return all(arr[i] < arr[i + 1] for i in range(len(arr) - 1))
+
+        n = len(nums)
+        count = 0
+        for start in range(n):
+            for end in range(start, n):
+                remaining = nums[:start] + nums[end + 1 :]
+                if is_strictly_increasing(remaining):
+                    count += 1
+
+        return count
