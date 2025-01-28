7fec2864993fdd29907b57d455b9358c7367dfb2
Sanjay <sutharsh0806@gmail.com>
2025-01-28 15:31:10 +0530

    russian doll envelopes

---
+++ Google/russian doll envelopes.py
@@ -0,0 +1,17 @@
+# Problem: Russian doll envelopes
+# Link: https://leetcode.com/problems/russian-doll-envelopes/
+
+
+class Solution:
+    def maxEnvelopes(self, e: List[List[int]]) -> int:
+        e.sort(key=lambda x: (x[0], -x[1]))
+        heights = [h for _, h in e]
+        lis = []
+        
+        for h in heights:
+            pos = bisect_left(lis, h)
+            if pos == len(lis):
+                lis.append(h) 
+            else:
+                lis[pos] = h  
+        return len(lis)
